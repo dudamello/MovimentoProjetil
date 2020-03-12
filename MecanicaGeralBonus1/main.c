bfbbbb86aa184ea67008aaf291956799df32b627
Eduarda Mello <duda.mello1@hotmail.com>
2020-03-12 11:06:38 -0300

    Initial Commit

---
+++ MecanicaGeralBonus1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MecanicaGeralBonus1
+//
+//  Created by Eduarda Mello on 12/03/20.
+//  Copyright © 2020 Eduarda Mello. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
